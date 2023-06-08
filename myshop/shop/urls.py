e983bf3f3f88a9f2804b065d4ce7d9c069363295
Paul <twinencrypt@gmail.com>
2023-06-08 13:50:25 +0700

    Add templates, static and media settings

--- myshop/shop/urls.py
+++ myshop/shop/urls.py
@@ -4,6 +4,6 @@ from shop import views
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
-    path('<slug:category_slag>/', views.product_list, name='product_list_by_category'),
+    path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
     path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
 ]
