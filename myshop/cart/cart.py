a611dd46e29e48cdef4014864eb7a04bf88352de
Paul <twinencrypt@gmail.com>
2023-06-09 15:53:11 +0700

    Add methods to cart

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -15,6 +15,9 @@ class Cart:
         self.cart = cart
 
     def add(self, product, quantity=1, override_quantity=False):
+        """
+         Добавить товар в корзину либо обновить его количество.
+         """
         product_id = str(product.id)
         if product_id not in self.cart:
             self.cart[product_id] = {'quantity': 0,
@@ -31,7 +34,41 @@ class Cart:
         self.session.modified = True
 
     def remove(self, product):
+        """
+        Удалить товар из корзины.
+        """
         product_id = str(product.id)
         if product_id in self.cart:
             del self.cart[product_id]
             self.save()
+
+    def __iter__(self):
+        """
+        Прокрутить товарные позиции корзины в цикле и
+        получить товары из базы данных.
+        """
+        product_ids = self.cart.keys()
+        # получить объекты product и добавить их в корзину
+        products = Product.objects.filter(id__in=product_ids)
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]['product'] = product
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
+
+    def __len__(self):
+        """
+         Подсчитать все товарные позиции в корзине.
+         """
+        return sum(item['quantity'] for item in self.cart.values())
+
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['quantity']
+                   for item in self.cart.values())
+
+    def clear(self):
+        # удалить корзину из сеанса
+        del self.session[settings.CART_SESSION_ID]
+        self.save()
